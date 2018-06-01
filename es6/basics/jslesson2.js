bc42ecf75ef83142a5a5d0b478f4e159c0dd3635
christymonisha <christymonisha8@gmail.com>
2018-06-01 11:56:47 +0530

    es6 operators

---
+++ es6/basics/jslesson2.js
@@ -0,0 +1,13 @@
+var x = 4;
+var y = -x;
+console.log('value of x is:',x);
+console.log("value of y is:",y);
+var z = !(x>10);
+console.log(z);
+var msg = "hi" + "ramya";
+console.log(msg);
+var result = x > 10 ? "true" : "false";
+console.log(result);
+console.log(typeof x);
+var r = "ramya";
+console.log(typeof r);
